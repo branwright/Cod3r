21c45a344e4353ccee143a0bd3f3bb5b45bbf319
Brandon Wright <bcmw2000@gmail.com>
2011-04-16 15:53:28 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cod3r::Application.config.secret_token = 'c82ddeac40227ecf389ce3443752d024d9ab5823db184bc7bb6412899350a7d64cc3354aa5dc8ecc1b56cbd1e9218f0679a325e7d37b605711a3f9363aaae6fb'
